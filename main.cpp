93632d3e3fa4b13d7f5642282b4f4c78baebb2a5
thepralad <prgurung04@gmail.com>
2023-11-20 12:19:39 +0530

    Refactored main.cpp

--- main.cpp
+++ main.cpp
@@ -2,89 +2,104 @@
 #include<vector>
 
 
-typedef struct userAndpass{
+struct userAndpass{
     std::string username;
     std::string password;
-}userAndpass;
+};
 
-std::string loginCode, userInputCode;
-int userChoice;
-bool Quit = false;
-std::vector<userAndpass> lists;
-
-void addPassword();
-void showPasswords();
+//Function declaration
+void addPassword(std::string loginCode, std::string userInputCode, std::vector<userAndpass>& PasswordList);
+void showPasswords(std::vector<userAndpass>& PasswordList);
 void title();
 
 int main(){
     
+    //Declaration of variables and vector
+    std::string loginCode, userInputCode;
+    std::vector<userAndpass> PasswordList;
+    int userChoice;
+    bool quit = false;
+
     std::cout << "Create password: ";
     std::getline(std::cin, loginCode);
     std::cout << "\033[1;44mPassword created successfully\033[0m" << std::endl;
     title();
-    while(!Quit){
+
+    //To loop until 'quit' is false
+    while (!quit){
         std::cout << "1. Add Password\n2. Show Passwords\n3. Exit" << std::endl;
         std::cout << "Enter: ";
         std::cin >> userChoice;
         std::cin.ignore();
+
+        //A switch-case for menu
         switch (userChoice){
         case 1:
-            addPassword();
+            addPassword(loginCode, userInputCode, PasswordList);
             break;
         case 2:
-            showPasswords();
+            showPasswords(PasswordList);
             break;
         case 3:
-        Quit = true;
+        quit = true;
             break;
         default:
-            std::cout << "Invalid Input\n";
+            std::cout << '\n';
+            std::cout << "\033[1;31mInvalid Input!\033[0m\n";
+            std::cout << '\n';
             break;
         }
     }
 
 }
-void addPassword(){
+
+//Function to add passwords
+void addPassword(std::string loginCode, std::string userInputCode, std::vector<userAndpass>& PasswordList){
     
+    //Declaration of variables
     userAndpass credentials;
     std::string username, password;
-    std::cout << "Enter Password: ";
-    std::getline(std::cin, userInputCode);
 
-    if (userInputCode != loginCode) {
+    //To check if password is correct, if not -recursion
+    do {
+        std::cout << "Enter Password: ";
+        std::getline(std::cin, userInputCode);
+        if(userInputCode != loginCode)
             std::cout << "\033[1;31mIncorrect Password! Try again.\033[0m" << std::endl;
-            addPassword();
     }
+    while (userInputCode != loginCode);
 
-    else{
+        //if correct, push credentials to the vector 'PasswordList'
         std::cout << "====================================\n";
         std::cout << "           ADD PASSWORDS\n";
         std::cout << "====================================\n";
+
         std::cout << "Username: ";
-        std::cin >> username;
+        std::getline(std::cin, credentials.username);
 
         std::cout << "Password: ";
-        std::cin >> password;
+        std::getline(std::cin, credentials.password);
 
-        credentials.username = username;
-        credentials.password = password;
-        
-        lists.push_back(credentials);
+        PasswordList.push_back(credentials);
 
         std::cout << "\033[1;44mPassword added successfully!\033[0m" << std::endl;
-    }
+    
     
 }
-void showPasswords(){
+
+//Function to display all the passwords
+void showPasswords(std::vector<userAndpass>& PasswordList){
     std::cout << "====================================\n";
     std::cout << "           YOUR PASSWORDS\n";
     std::cout << "====================================\n";
 
-    for(userAndpass e : lists){
-        std::cout << "Username: " << e.username << "| Password: " << e.password << std::endl;
+    for (const auto& entry : PasswordList){
+        std::cout << "Username: " << entry.username << "| Password: " << entry.password << std::endl;
     }
     std::cout << "====================================\n";
 }
+
+//TItle
 void title(){
    std::cout << "  ____                                                      _     __  __                                                \n"
                  " |  _ \\    __ _   ___   ___  __      __   ___    _ __    __| |   |  \\/  |   __ _   _ __     __ _    __ _    ___   _ __ \n"
