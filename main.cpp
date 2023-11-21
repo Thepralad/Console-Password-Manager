c9de4ae226ff312b034939ab73473b7134f7a93a
thepralad <prgurung04@gmail.com>
2023-11-21 20:00:02 +0530

    Refactored the code to more modular

--- main.cpp
+++ main.cpp
@@ -1,31 +1,40 @@
 #include<iostream>
 #include<vector>
 
-
 struct userAndpass{
-    std::string username;
-    std::string password;
+        std::string username;
+        std::string password;
 };
+class PasswordManager{
+public:
+    void run();
 
-//Function declaration
-void addPassword(std::string loginCode, std::string userInputCode, std::vector<userAndpass>& PasswordList);
-void showPasswords(std::vector<userAndpass>& PasswordList);
-void title();
-
-int main(){
-    
-    //Declaration of variables and vector
-    std::string loginCode, userInputCode;
+private:
+    std::string loginCode; 
+    std::string userInputCode;
     std::vector<userAndpass> PasswordList;
-    int userChoice;
-    bool quit = false;
+   
+    void addPassword();
+    void showPasswords();
+    void title();
+
+};
+int main(){
+    PasswordManager manager;
+    manager.run();
+    return 1;
+}
+
+void PasswordManager::run(){
 
     std::cout << "Create password: ";
     std::getline(std::cin, loginCode);
     std::cout << "\033[1;44mPassword created successfully\033[0m" << std::endl;
     title();
 
-    //To loop until 'quit' is false
+    int userChoice;
+    bool quit = false;
+
     while (!quit){
         std::cout << "1. Add Password\n2. Show Passwords\n3. Exit" << std::endl;
         std::cout << "Enter: ";
@@ -35,10 +44,10 @@ int main(){
         //A switch-case for menu
         switch (userChoice){
         case 1:
-            addPassword(loginCode, userInputCode, PasswordList);
+            addPassword();
             break;
         case 2:
-            showPasswords(PasswordList);
+            showPasswords();
             break;
         case 3:
         quit = true;
@@ -50,13 +59,9 @@ int main(){
             break;
         }
     }
-
 }
 
-//Function to add passwords
-void addPassword(std::string loginCode, std::string userInputCode, std::vector<userAndpass>& PasswordList){
-    
-    //Declaration of variables
+void PasswordManager::addPassword(){
     userAndpass credentials;
     std::string username, password;
 
@@ -66,19 +71,34 @@ void addPassword(std::string loginCode, std::string userInputCode, std::vector<u
         std::getline(std::cin, userInputCode);
         if(userInputCode != loginCode)
             std::cout << "\033[1;31mIncorrect Password! Try again.\033[0m" << std::endl;
-    }
-    while (userInputCode != loginCode);
+
+    } while (userInputCode != loginCode);
 
         //if correct, push credentials to the vector 'PasswordList'
         std::cout << "====================================\n";
         std::cout << "           ADD PASSWORDS\n";
         std::cout << "====================================\n";
 
-        std::cout << "Username: ";
-        std::getline(std::cin, credentials.username);
+        do {
+            std::cout << "Username: ";
+            std::getline(std::cin, credentials.username);
 
-        std::cout << "Password: ";
-        std::getline(std::cin, credentials.password);
+            if(credentials.username == ""){
+                std::cout << "\033[1;31mUsername cannot be empty!\033[0m" << std::endl;
+            }
+
+        }while(credentials.username == "");
+
+        do{
+            std::cout << "Password: ";
+            std::getline(std::cin, credentials.password);
+
+            if(credentials.password == ""){
+                std::cout << "\033[1;31mPassword cannot be empty!\033[0m" << std::endl;
+            }
+
+        } while(credentials.password == "");
+        
 
         PasswordList.push_back(credentials);
 
@@ -87,8 +107,7 @@ void addPassword(std::string loginCode, std::string userInputCode, std::vector<u
     
 }
 
-//Function to display all the passwords
-void showPasswords(std::vector<userAndpass>& PasswordList){
+void PasswordManager::showPasswords(){
     std::cout << "====================================\n";
     std::cout << "           YOUR PASSWORDS\n";
     std::cout << "====================================\n";
@@ -99,8 +118,7 @@ void showPasswords(std::vector<userAndpass>& PasswordList){
     std::cout << "====================================\n";
 }
 
-//TItle
-void title(){
+void PasswordManager::title(){
    std::cout << "  ____                                                      _     __  __                                                \n"
                  " |  _ \\    __ _   ___   ___  __      __   ___    _ __    __| |   |  \\/  |   __ _   _ __     __ _    __ _    ___   _ __ \n"
                  " | |_) |  / _` | / __| / __| \\ \\ /\\ / /  / _ \\  | '__|  / _` |   | |\\/| |  / _` | | '_ \\   / _` |  / _` |  / _ \\ | '__|\n"
